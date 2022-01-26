66f84e9b1541ae0c3c294f7a38e7c2d5b0d0a1f3
zden3k <tomek.szpyrko@gmail.com>
2022-01-26 12:53:31 +0000

    offline test json off

--- app/Http/Controllers/CharacterController.php
+++ app/Http/Controllers/CharacterController.php
@@ -13,15 +13,15 @@ class CharacterController extends Controller
      */
     public function index()
     {
-        $response = ('{"info":
-        {"count":826,"pages":42,"next":"https://rickandmortyapi.com/api/character/?page=2","prev":null},
-        "results":[
-        {"id":1,"name":"Rick Sanchez","status":"Alive","species":"Human","type":"","gender":"Male","origin":
-        {"name":"Earth (C-137)","url":"https://rickandmortyapi.com/api/location/1"},
-        "location":{"name":"Citadel of Ricks","url":"https://rickandmortyapi.com/api/location/3"},
-        "image":"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
-        "episode":["https://rickandmortyapi.com/api/episode/1","https://rickandmortyapi.com/api/episode/2"],
-        "url":"https://rickandmortyapi.com/api/character/2","created":"2017-11-04T18:50:21.651Z"}]}');
+//        $response = ('{"info":
+//        {"count":826,"pages":42,"next":"https://rickandmortyapi.com/api/character/?page=2","prev":null},
+//        "results":[
+//        {"id":1,"name":"Rick Sanchez","status":"Alive","species":"Human","type":"","gender":"Male","origin":
+//        {"name":"Earth (C-137)","url":"https://rickandmortyapi.com/api/location/1"},
+//        "location":{"name":"Citadel of Ricks","url":"https://rickandmortyapi.com/api/location/3"},
+//        "image":"https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+//        "episode":["https://rickandmortyapi.com/api/episode/1","https://rickandmortyapi.com/api/episode/2"],
+//        "url":"https://rickandmortyapi.com/api/character/2","created":"2017-11-04T18:50:21.651Z"}]}');
         //$response = Http::get('https://rickandmortyapi.com/api/character/');
         $page = (string)(isset($_GET['page']) ? "?page=" . ($_GET['page']) : '');
         $url = urlencode( 'https://rickandmortyapi.com/api/character/' . $page);
